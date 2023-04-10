b16406892eb325f7d4be883e567e0227024d9dd0
dung <dungk21a1@gmail.com>
2023-04-10 16:24:47 +0700

    first commit

---
+++ security/src/main/java/com/dung/security/user/Role.java
@@ -0,0 +1,8 @@
+package com.dung.security.user;
+
+public enum Role {
+
+    USER,
+
+    ADMIN
+}
---
+++ security/src/test/java/com/dung/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dung.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
